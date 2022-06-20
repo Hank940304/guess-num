22ab9817b38055aa96b567536d073f14d766f1a8
Hank <hank.zhtw@gmail.com>
2022-06-20 16:51:20 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1, 100)
+start = input('請決定隨機數字範圍開始值: ')
+end = input('請決定隨機數字範圍結束值: ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 while:
 	count = count + 1
 	num = input('請猜數字: ')
